8d43ec8d850111dc75c0ab3638eef8ea43e2e96c
IsadoraMarin <icarolinamaring@gmail.com>
2025-11-21 16:32:55 -0300

    Wiwiwi

--- pasteleria-react-main/src/componentes/atoms/Colors.js
+++ pasteleria-react-main/src/componentes/atoms/Colors.js
@@ -1,4 +1,4 @@
 import colors from './Colores.js';
-
+//Por alguna razon las pruebas no funcionaban si no tenian ingles, no se, solo queria que saliese el Success
 export default colors;
 export const { primary, secondary, accent, dark, light } = colors;
